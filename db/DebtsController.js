2e062dafb43ca45d8d5a86081db3f82bec9e453d
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-05-13 22:31:36 -0400

    CRUD functionality added and completed

--- db/DebtsController.js
+++ db/DebtsController.js
@@ -14,4 +14,43 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/name/:name", (req, res) => {
+  DebtsModel.find({ name: req.params.name })
+    .then(debt => {
+      res.json(debt);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
+
+router.post("/newDebt", (req, res) => {
+  DebtsModel.create(req.body)
+    .then(debt => {
+      res.json(debt);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
+
+router.put("/update/:name", (req, res) => {
+  DebtsModel.update({ name: req.params.name }, req.body)
+    .then(debt => {
+      res.json(debt);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
+
+router.delete("/delete/:name", (req, res) => {
+  DebtsModel.deleteOne({ name: req.params.name })
+    .then(debt => {
+      res.json(debt);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
 module.exports = router;
