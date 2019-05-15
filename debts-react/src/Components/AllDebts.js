00b4424ffe55a8e2cfc62d394a64764a6b76acfe
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-05-14 23:19:28 -0400

    added two more

--- debts-react/src/Components/AllDebts.js
+++ debts-react/src/Components/AllDebts.js
@@ -8,12 +8,14 @@ class AllDebts extends Component {
       return (
         <div className="debtCard">
           <img src={debt.picture} alt={debt.name} />
+          <hr />
           <h1>{debt.name}</h1>
-          <p className="title">Proffesion and title</p>
           <p>Owes Me: ${debt.moneyOwed}</p>
-          <p>{debt.reason}</p>
-          <p>{debt.interestRate}</p>
-          <p>{debt.dueDate}</p>
+          <p>Reason for debt: {debt.reason}</p>
+          <p>Interest Rate: {debt.interestRate}</p>
+          <p>Due Date: {debt.dueDate}</p>
+          <p>Phone Number: {debt.phoneNumber}</p>
+          <p>Email: {debt.email}</p>
           <button onClick={() => this.props.delete}>Paid Off</button>
         </div>
       );
