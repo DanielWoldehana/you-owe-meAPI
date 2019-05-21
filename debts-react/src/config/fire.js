b12f3d54dbc403c1941192f32e91754765e67a61
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-05-21 18:38:33 -0400

    changed auth key

--- debts-react/src/config/fire.js
+++ debts-react/src/config/fire.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJVvBZmQNh1Eo0tlZJz9_gBOzJ3wajqr4",
-  authDomain: "trips-api.firebaseapp.com",
-  databaseURL: "https://trips-api.firebaseio.com",
-  projectId: "trips-api",
-  storageBucket: "trips-api.appspot.com",
-  messagingSenderId: "671816341719",
-  appId: "1:671816341719:web:525e9c74efe194e0"
+  apiKey: "AIzaSyAZrlso3sl2fkPau8u-MC22duPfGDvLKxQ",
+  authDomain: "alldebts-api.firebaseapp.com",
+  databaseURL: "https://alldebts-api.firebaseio.com",
+  projectId: "alldebts-api",
+  storageBucket: "alldebts-api.appspot.com",
+  messagingSenderId: "905212713418",
+  appId: "1:905212713418:web:d0c8aaf2ac6caf7e"
 };
 
 const fire = firebase.initializeApp(firebaseConfig);
